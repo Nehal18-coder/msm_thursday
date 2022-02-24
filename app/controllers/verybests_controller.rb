4c34c1877268b4fe4b8d8d84734d8af357ac19b1
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-02-24 05:17:56 +0000

    Remove controller comments

--- app/controllers/verybests_controller.rb
+++ app/controllers/verybests_controller.rb
@@ -1,24 +1,19 @@
 class VerybestsController < ApplicationController
   before_action :set_verybest, only: %i[show edit update destroy]
 
-  # GET /verybests
   def index
     @q = Verybest.ransack(params[:q])
     @verybests = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
-  # GET /verybests/1
   def show; end
 
-  # GET /verybests/new
   def new
     @verybest = Verybest.new
   end
 
-  # GET /verybests/1/edit
   def edit; end
 
-  # POST /verybests
   def create
     @verybest = Verybest.new(verybest_params)
 
@@ -29,7 +24,6 @@ class VerybestsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /verybests/1
   def update
     if @verybest.update(verybest_params)
       redirect_to @verybest, notice: "Verybest was successfully updated."
@@ -38,7 +32,6 @@ class VerybestsController < ApplicationController
     end
   end
 
-  # DELETE /verybests/1
   def destroy
     @verybest.destroy
     redirect_to verybests_url, notice: "Verybest was successfully destroyed."
@@ -46,12 +39,10 @@ class VerybestsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_verybest
     @verybest = Verybest.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def verybest_params
     params.fetch(:verybest, {})
   end
